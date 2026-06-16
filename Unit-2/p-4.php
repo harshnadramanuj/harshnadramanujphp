e7974203c97521da3b73eac0c8fe4e5847253f8c
harshnadramanuj <harshnad.ramanuj130047@marwadiuniversity.ac.in>
2026-06-16 10:07:13 +0530

    Add files via upload

---
+++ Unit-2/p-4.php
@@ -0,0 +1,27 @@
+<?php
+
+// 1. Change keys to uppercase
+print_r(array_change_key_case(["a" => 1, "b" => 2], CASE_UPPER));
+
+// 2. Split months into groups of 2
+print_r(array_chunk(["Jan", "Feb", "Mar", "Apr"], 2));
+
+// 3. Count frequencies of values
+print_r(array_count_values(["X", "Y", "X", "Z", "Y"]));
+
+// 4. Combine keys and values
+print_r(array_combine(["id", "name"], [101, "Alex"]));
+
+// 5. Remove last item
+$arr5 = [1, 2, 3]; array_pop($arr5); print_r($arr5);
+
+// 6. Add to the end
+$arr6 = [1, 2]; array_push($arr6, 3, 4); print_r($arr6);
+
+// 7. Add to the start
+$arr7 = [2, 3]; array_unshift($arr7, 1); print_r($arr7);
+
+// 8. Remove first item
+$arr8 = [1, 2, 3]; array_shift($arr8); print_r($arr8);
+
+?>
